36cb947646a38b83bb638df079ae3ee5d034c5dc
GustavoLorena <113564100+GustavoLorena@users.noreply.github.com>
2023-08-07 19:50:07 -0300

    Add files via upload

---
+++ Objetos/aula-1/objeto.js
@@ -0,0 +1,11 @@
+// const listaCPFs = [1111111, 2222222, 3333333];
+
+// const informacoesPessoa = [ "nome", "Jose", "idade", 32, "CPF", "11122233344"]
+
+// console.log(informacoesPessoa[1])
+
+const objetoPessoa = {
+    nome: "Jose",
+    idade: 32,
+};
+console.log(nome);
