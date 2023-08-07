9ac030bcdb299640c40b513355ad61d05d9dfb38
GustavoLorena <113564100+GustavoLorena@users.noreply.github.com>
2023-08-07 19:56:20 -0300

    Create index.js

---
+++ Node - Arquivos iniciais/index.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('olá mundo'))
+
+console.log('o, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).');
+console.log('olá mundo');
